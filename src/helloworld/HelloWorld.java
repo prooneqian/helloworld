e58700a7a5b1e7dd163c2ea0bf8d7eb5227250aa
qian <qian@DESKTOP-HEA527Q>
2019-03-20 11:23:59 +0800

    version0.2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello WorldÐÞ¸Ä¹ý¹þ¹þ¹þ");
     }
 }
